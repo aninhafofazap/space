b64542dba89fdc5686360ecac6fcf7c626445f16
Ana Victoria <anavictoriagomes1810@gmail.com>
2023-02-01 18:53:09 -0300

    Feat: Creating photo section

--- src/Componentes/Gallery/index.jsx
+++ src/Componentes/Gallery/index.jsx
@@ -1,5 +1,10 @@
 import React from "react";
 import Tags from "../Tags";
+import styles from "./gallery.module.scss"
+import open from "./open.png"
+import favorito from "./favorito.png";
+
+import fotos from './fotos.json';
 
 function Galeria(){
     return (
@@ -9,6 +14,25 @@ function Galeria(){
             </h2>
             <Tags/>
             <ul>
+                {
+                    fotos.map((foto => {
+                        return (
+                            <li key={foto.id} className={styles.galeria__card}>
+                                <img 
+                                className={styles.galeria__imagem} src={foto.imagem} alt={foto.titulo}
+                                />
+                                <p className={styles.galeria__descricao}>{foto.titulo}</p>
+                                <div>
+                                    <p>{foto.creditos}</p>
+                                    <span>
+                                        <img src={favorito} alt="ícone favoritar"/>
+                                        <img src={open} alt="ícone abrir modal"/>
+                                    </span>
+                                </div>
+                            </li>   
+                        );
+                    }))
+                }
             </ul>
         </section>
     )
